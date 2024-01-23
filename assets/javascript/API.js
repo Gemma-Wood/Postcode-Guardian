3f538198e2f33fe93c1b4e48fcf6e24409177f3f
eatontechnology <eaton.technology@gmail.com>
2024-01-23 18:41:58 +0000

    postcode data prints to page

--- assets/javascript/API.js
+++ assets/javascript/API.js
@@ -1,8 +1,9 @@
 // JavaScript file dedicated only to API
 // Insert endpoint URL
 const postcodeAPI = "https://api.postcodes.io/postcodes/";
-//the crime API at the moment extracts crime data froma specific location and date. Can change later if we want
-const crimeAPI = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2017-01";
+//we need a replica of the above API in order to serve the crime API, as the crime API is not specific enough.
+const postcodeCrime = "https://api.postcodes.io/postcodes/<postcode>"
+const crimeAPI = "https://data.police.uk/api/crimes-street/all-crime?lat=LAT_HERE&lng=LONG_HERE";
 
 // Function to fetch postcode data
 function fetchPostcodeData() {
@@ -28,10 +29,13 @@ function fetchPostcodeInfo(postcode) {
             // Check if 'postcodeInfo' is not null or undefined before updating the HTML
             if (postcodeInfo !== null && postcodeInfo !== undefined) {
                 // Update the HTML with the fetched data
-                document.getElementById('regionData').innerHTML = `Region: ${postcodeInfo.region}`;
-                document.getElementById('districtData').innerHTML = `Admin District: ${postcodeInfo.admin_district}`;
-                document.getElementById('wardData').innerHTML = `Admin Ward: ${postcodeInfo.admin_ward}`;
-                document.getElementById('parliamentData').innerHTML = `Parliamentary Constituency: ${postcodeInfo.parliamentary_constituency}`;
+                document.getElementById('regionData').innerHTML = `<strong>Region:</strong><br> ${postcodeInfo.region}`;
+                document.getElementById('districtData').innerHTML = `<strong>Admin District:</strong><br> ${postcodeInfo.admin_district}`;
+                document.getElementById('wardData').innerHTML = `<strong>Admin Ward:</strong><br> ${postcodeInfo.admin_ward}`;
+                document.getElementById('parliamentData').innerHTML = `<strong>Parliamentary Constituency:</strong><br> ${postcodeInfo.parliamentary_constituency}`;
+
+                // Fetch crime data using the obtained latitude and longitude
+                fetchCrimeData(postcodeInfo.latitude, postcodeInfo.longitude);
             } else {
                 // Handle the case when 'postcodeInfo' is null or undefined
                 console.error('Postcode information is null or undefined.');
@@ -39,10 +43,6 @@ function fetchPostcodeInfo(postcode) {
         })
         .catch(error => {
             console.error('Error fetching postcode data:', error);
-            // You can handle errors here, e.g., display a message to the user
         });
 }
-
-// Attach the fetchPostcodeData function to the search button
-document.getElementById('searchButton').addEventListener('click', fetchPostcodeData);
 
